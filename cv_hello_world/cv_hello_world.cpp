23b1cf3e2a545eca077ce5627b1df53ce242c0e5
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2018-06-07 12:11:39 -0500

    added hello world example

---
+++ cv_hello_world/cv_hello_world.cpp
@@ -0,0 +1,65 @@
+//
+//    Copyright 2018 Christopher D. McMurrough
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/*******************************************************************************************************************//**
+ * @file cv_hello_world.cpp
+ * @brief C++ hello world example for OpenCV
+ * @author Christopher D. McMurrough
+ **********************************************************************************************************************/
+
+// include necessary dependencies
+#include <iostream>
+#include <cstdio>
+#include "opencv2/opencv.hpp"
+
+// configuration parameters
+#define NUM_COMNMAND_LINE_ARGUMENTS 1
+#define DISPLAY_WINDOW_NAME "Hello world!"
+
+/*******************************************************************************************************************//**
+ * @brief program entry point
+ * @param[in] argc number of command line arguments
+ * @param[in] argv string array of command line arguments
+ * @return return code (0 for normal termination)
+ * @author Christoper D. McMurrough
+ **********************************************************************************************************************/
+int main(int argc, char **argv)
+{
+    // validate and parse the command line arguments
+    if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
+    {
+        std::printf("USAGE: %s <image_path> \n", argv[0]);
+        return 0;
+    }
+    else
+    {
+		cv::Mat imageIn;
+		imageIn = cv::imread(argv[1], cv::CV_LOAD_IMAGE_COLOR);
+		
+		// check for file error
+		if(!image.data)
+		{
+			std::cout << "Error while opening file " << argv[1] << std::endl;
+			return 0;
+		}
+		else
+		{
+			cv::imshow(DISPLAY_WINDOW_NAME, imageIn);
+			cv::waitKey();
+		}
+    }
+}
