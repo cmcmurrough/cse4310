8e1e5e6ca998d4f47a3c13e02f27e1f290f8992a
cmcmurrough <christopher.mcmurrough@gmail.com>
2021-06-23 19:08:44 +0000

    refactor

--- cv_lines/cv_lines.cpp
+++ cv_lines/cv_lines.cpp
@@ -85,7 +85,7 @@ int main(int argc, char **argv)
     for( size_t i = 0; i < lines.size(); i++ )
     {
         cv::Vec4i l = lines[i];
-        cv::line(imageIn, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 0, 255), 3, CV_AA);
+        cv::line(imageIn, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
     }
 
     // display the images
