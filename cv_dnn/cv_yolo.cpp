7f1848cc41829413468de17bb97dd5f12b11c5cc
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2021-07-19 11:33:59 -0500

    release update for cv_yolo.cpp

--- cv_dnn/cv_yolo.cpp
+++ cv_dnn/cv_yolo.cpp
@@ -15,7 +15,7 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-/*******************************************************************************************************************//**
+/*******************************************************************************************************************/ /**
  * @file cv_yolo.cpp
  * @brief C++ example for processing frames with Yolo using OpenCV DNN module
  * @author Christopher D. McMurrough
@@ -36,7 +36,7 @@
 // declare function prototypes
 bool processFrame(const cv::Mat &imageIn, cv::Mat &imageOut);
 
-/*******************************************************************************************************************//**
+/*******************************************************************************************************************/ /**
  * @brief Process a single image frame
  * @param[in] imageIn the input image frame
  * @param[out] imageOut the processed image frame
@@ -49,100 +49,73 @@ bool processFrame(const cv::Mat &imageIn, cv::Mat &imageOut, cv::dnn::Net &netwo
     // copy the input image frame to the ouput image (deep copy)
     imageOut = imageIn.clone();
 
+    // create the input DNN blob from the image frame
+    static cv::Mat blobFromImg;
+    const double scaleFactor = 1.0;
+    const cv::Size size = cv::Size(416, 416);
+    const cv::Scalar mean = cv::Scalar();
+    const bool swapRB = false;
+    const bool crop = false;
+    const int ddepth = cv::CV_32F;
+    cv::dnn::blobFromImage(imageIn, blobFromImg, scaleFactor, size, mean, swapRB, crop);
 
+    // set the blob as input to the network
+    float scale = 1.0 / 255.0;
+    cv::Scalar mean = 0;
+    network.setInput(blobFromImg, "", scale, mean);
 
+    // feed forward the inputs through the network
+    cv::Mat outMat;
+    network.forward(outMat);
 
+    // reach row represents one result per detected object
+    int numRows = outMat.rows;
 
+    // column arrangement:
+    // [x, y, w, h, class_1_score, class_2_score, ..., ... class_N_score]
+    int numCols = outMat.cols;
+    for (int j = 0; j < numRows; ++j)
+    {
+        // get scores for each possible class (starting at element 5)
+        cv::Mat scores = outMat.row(j).colRange(5, numCols);
 
+        // find indexes of min and max confidence and related index of element.
+        cv::Point maxPos;
+        double confidence;
+        cv::minMaxLoc(scores, 0, &confidence, 0, &maxPos);
 
+        // consider only scores above a minimum threshold
+        const double minConfidence = 0.0001;
+        if (confidence > minConfidence)
+        {
+            // parse the coordinates and parameters of this result
+            int center_x = (int)(outMat.at<float>(j, 0) * imageOut.cols);
+            int center_y = (int)(outMat.at<float>(j, 1) * imageOut.rows);
+            int width = (int)(outMat.at<float>(j, 2) * imageOut.cols + 20);
+            int height = (int)(outMat.at<float>(j, 3) * imageOut.rows + 100);
 
+            // calculate top left
+            int left = center_x - width / 2;
+            int top = center_y - height / 2;
 
-
-
-		static cv::Mat blobFromImg;
-        bool swapRB = true;
-        cv::dnn::blobFromImage(imageIn, blobFromImg, 1, cv::Size(416, 416), cv::Scalar(), swapRB, false);
-        //std::cout << blobFromImg.size() << endl; 
-        
-        float scale = 1.0 / 255.0;
-        cv::Scalar mean = 0;
-        network.setInput(blobFromImg, "", scale, mean);
-
-        cv::Mat outMat;
-        network.forward(outMat);
-            // rows represent number of detected object (proposed region)
-            int rowsNoOfDetection = outMat.rows;
-
-            // The coluns looks like this, The first is region center x, center y, width
-            // height, The class 1 - N is the column entries, which gives you a number, 
-            // where the biggist one corrsponding to most probable class. 
-            // [x ; y ; w; h; class 1 ; class 2 ; class 3 ;  ; ;....]
-            //  
-            int colsCoordinatesPlusClassScore = outMat.cols;
-            // Loop over number of detected object. 
-            for (int j = 0; j < rowsNoOfDetection; ++j)
-            {
-                // for each row, the score is from element 5 up
-                // to number of classes index (5 - N columns)
-                cv::Mat scores = outMat.row(j).colRange(5, colsCoordinatesPlusClassScore);
-
-                cv::Point PositionOfMax;
-                double confidence;
-
-                // This function find indexes of min and max confidence and related index of element. 
-                // The actual index is match to the concrete class of the object.
-                // First parameter is Mat which is row [5fth - END] scores,
-                // Second parameter will gives you min value of the scores. NOT needed 
-                // confidence gives you a max value of the scores. This is needed, 
-                // Third parameter is index of minimal element in scores
-                // the last is position of the maximum value.. This is the class!!
-                cv::minMaxLoc(scores, 0, &confidence, 0, &PositionOfMax);
-            
-                if (confidence > 0.0001)
-                {
-// thease four lines are
-// [x ; y ; w; h;
-                    int centerX = (int)(outMat.at<float>(j, 0) * imageOut.cols); 
-                    int centerY = (int)(outMat.at<float>(j, 1) * imageOut.rows); 
-                    int width =   (int)(outMat.at<float>(j, 2) * imageOut.cols+20); 
-                   int height =   (int)(outMat.at<float>(j, 3) * imageOut.rows+100); 
-
-                    int left = centerX - width / 2;
-                    int top = centerY - height / 2;
-
-
-                    std::stringstream ss;
-                    ss << PositionOfMax.x;
-                    std::string clas = ss.str();
-                    int color = PositionOfMax.x * 10;
-                    cv::putText(imageOut, clas, cv::Point(left, top), 1, 2, cv::Scalar(color, 255, 255), 2, false);
-                    std::stringstream ss2;
-                    ss << confidence;
-                    std::string conf = ss.str();
-
-                    cv::rectangle(imageOut, cv::Rect(left, top, width, height), cv::Scalar(color, 0, 0), 2, 8, 0);
-                }
-            }
-			
-			
-			
-			
-			
-			
-			
+            // annotate the image
+            std::stringstream ss;
+            ss << maxPos.x;
+            std::string clas = ss.str();
+            int color = maxPos.x * 10;
+            cv::putText(imageOut, clas, cv::Point(left, top), 1, 2, cv::Scalar(color, 255, 255), 2, false);
+            std::stringstream ss2;
+            ss << confidence;
+            std::string conf = ss.str();
+            cv::rectangle(imageOut, cv::Rect(left, top, width, height), cv::Scalar(color, 0, 0), 2, 8, 0);
+        }
+    }
 
     // return true on success
     return true;
 }
 
-
-
-
-
-
-
-
-/*******************************************************************************************************************//**
+/*******************************************************************************************************************/ /**
  * @brief program entry point
  * @param[in] argc number of command line arguments
  * @param[in] argv string array of command line arguments
@@ -155,7 +128,7 @@ int main(int argc, char **argv)
     std::string videoFileName;
 
     // validate and parse the command line arguments
-    if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
+    if (argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
     {
         std::printf("USAGE: %s <file_path> \n", argv[0]);
         return 0;
@@ -167,19 +140,18 @@ int main(int argc, char **argv)
 
     // open the video file
     cv::VideoCapture capture(videoFileName);
-    if(!capture.isOpened())
+    if (!capture.isOpened())
     {
         std::printf("Unable to open video source, terminating program! \n");
         return 0;
     }
-	
-	// initialize YOLO
-	std::string model = "yolov3-tiny.weights";
-    std::string config = "yolov3-tiny.cfg"; 
-    cv::dnn::Net network = cv::dnn::readNet(model, config,"Darknet");
+
+    // initialize YOLO
+    std::string model = "yolov3-tiny.weights";
+    std::string config = "yolov3-tiny.cfg";
+    cv::dnn::Net network = cv::dnn::readNet(model, config, "Darknet");
     network.setPreferableBackend(cv::dnn::DNN_BACKEND_DEFAULT);
     network.setPreferableTarget(cv::dnn::DNN_TARGET_OPENCL);
-	
 
     // get the video source parameters
     int captureWidth = static_cast<int>(capture.get(cv::CAP_PROP_FRAME_WIDTH));
@@ -193,7 +165,7 @@ int main(int argc, char **argv)
     // process data until program termination
     bool doCapture = true;
     int frameCount = 0;
-    while(doCapture)
+    while (doCapture)
     {
         // get the start time
         double startTicks = static_cast<double>(cv::getTickCount());
@@ -202,7 +174,7 @@ int main(int argc, char **argv)
         cv::Mat captureFrame;
         cv::Mat processedFrame;
         bool captureSuccess = capture.read(captureFrame);
-        if(captureSuccess)
+        if (captureSuccess)
         {
             // process the image frame
             processFrame(captureFrame, processedFrame, network);
@@ -216,7 +188,7 @@ int main(int argc, char **argv)
         }
 
         // update the GUI window if necessary
-        if(captureSuccess)
+        if (captureSuccess)
         {
             cv::imshow(DISPLAY_WINDOW_NAME, processedFrame);
 
@@ -224,7 +196,7 @@ int main(int argc, char **argv)
             int delayMs = (1.0 / captureFPS) * 1000;
 
             // check for program termination
-            if(((char) cv::waitKey(1)) == 'q')
+            if (((char)cv::waitKey(1)) == 'q')
             {
                 doCapture = false;
             }
