dd174dd400d81ce328e6eeb53cb159f013b55dae
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2021-07-20 20:53:54 -0500

    Create download_models.sh

---
+++ cv_dnn/download_models.sh
@@ -0,0 +1,5 @@
+# download  YOLOv3-tiny models
+
+# download MaskRCNN models
+wget http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
+tar zxvf mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
