eb284f7898928b26a044055e9541006a2da0ddcd
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2021-07-20 21:14:18 -0500

    Update download_models.sh

--- cv_dnn/download_models.sh
+++ cv_dnn/download_models.sh
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.c
 # download MaskRCNN models
 wget http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
 tar zxvf mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
+wget https://raw.githubusercontent.com/opencv/opencv_extra/master/testdata/dnn/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt
